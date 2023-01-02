49ed6683ededf09d076057dd8a8cf8dd28c8ffd6
Zak <zak.johansson@gmail.com>
2023-01-02 10:45:38 -0800

    create register route for server

--- routes/register.js
+++ routes/register.js
@@ -1,3 +1,26 @@
 const express = require("express");
 const cookieSession = require("cookie-session");
 const bcrypt = require("bcryptjs");
+
+const app = express();
+
+app.post("/register", (req, res) => {
+  let id = "";
+  id += generateRandomString();
+  req.session.user_id = id;
+
+  const email = req.body.email;
+  const password = req.body.password;
+  const hashedPassword = bcrypt.hashSync(password);
+  const foundEmail = emailFind(email, users);
+
+  if (email === "" || password === "") {
+    res.sendStatus(400);
+  }
+  if (foundEmail === email) {
+    res.sendStatus(400);
+  }
+
+  users[id] = { id, email, hashedPassword };
+  res.redirect("/urls");
+});
