230b779dc87aee8f84f5cf64d7034ba715b47891
Zak <zak.johansson@gmail.com>
2023-01-03 11:05:46 -0800

    create working register route

--- routes/register.js
+++ routes/register.js
@@ -1,28 +1,35 @@
 const express = require("express");
-//const cookieSession = require("cookie-session");
-const bcrypt = require("bcryptjs");
-
 const app = express();
+const router = express.Router();
 
-app.use;
+//const cookieSession = require("cookie-session");
+// const bcrypt = require("bcryptjs");
 
-app.post("/register", (req, res) => {
-  let id = "";
-  id += generateRandomString();
-  req.session.user_id = id;
+app.set("view engine", "ejs");
 
-  const email = req.body.email;
-  const password = req.body.password;
-  const hashedPassword = bcrypt.hashSync(password);
-  const foundEmail = emailFind(email, users);
-
-  if (email === "" || password === "") {
-    res.sendStatus(400);
-  }
-  if (foundEmail === email) {
-    res.sendStatus(400);
-  }
-
-  users[id] = { id, email, hashedPassword };
-  res.redirect("/urls");
+router.get("/quizapp/login/new", (req, res) => {
+  res.render("register");
 });
+
+// app.post("/register", (req, res) => {
+//   let id = "";
+//   id += generateRandomString();
+//   req.session.user_id = id;
+
+//   const email = req.body.email;
+//   const password = req.body.password;
+//   const hashedPassword = bcrypt.hashSync(password);
+//   const foundEmail = emailFind(email, users);
+
+//   if (email === "" || password === "") {
+//     res.sendStatus(400);
+//   }
+//   if (foundEmail === email) {
+//     res.sendStatus(400);
+//   }
+
+//   users[id] = { id, email, hashedPassword };
+//   res.redirect("/urls");
+// });
+
+module.exports = router;
