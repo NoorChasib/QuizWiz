a6962a84a4c3e9d5a36af1d28bad947828db800c
Zak <zak.johansson@gmail.com>
2023-01-03 14:40:40 -0800

    add login route

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ const userApiRoutes = require("./routes/users-api");
 const widgetApiRoutes = require("./routes/widgets-api");
 const usersRoutes = require("./routes/users");
 const registerRoutes = require("./routes/register");
+const loginRoutes = require("./routes/login");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
@@ -40,6 +41,7 @@ app.use("/api/users", userApiRoutes);
 app.use("/api/widgets", widgetApiRoutes);
 app.use("/user", usersRoutes);
 app.use(registerRoutes);
+app.use(loginRoutes);
 // Note: mount other resources here, using the same pattern above
 
 // Home page
