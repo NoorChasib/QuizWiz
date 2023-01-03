0ea7a8c7ed5431b834f8ccacc66f4a7bcc77c3bc
Zak <zak.johansson@gmail.com>
2023-01-03 12:14:40 -0800

    create postgres connection file

--- routes/register.js
+++ routes/register.js
@@ -11,25 +11,25 @@ router.get("/quizapp/login/new", (req, res) => {
   res.render("register");
 });
 
-// app.post("/register", (req, res) => {
-//   let id = "";
-//   id += generateRandomString();
-//   req.session.user_id = id;
+app.post("/register", (req, res) => {
+  let id = "";
+  id += generateRandomString();
+  req.session.user_id = id;
 
-//   const email = req.body.email;
-//   const password = req.body.password;
-//   const hashedPassword = bcrypt.hashSync(password);
-//   const foundEmail = emailFind(email, users);
+  const email = req.body.email;
+  const password = req.body.password;
+  const hashedPassword = bcrypt.hashSync(password);
+  const foundEmail = emailFind(email, users);
 
-//   if (email === "" || password === "") {
-//     res.sendStatus(400);
-//   }
-//   if (foundEmail === email) {
-//     res.sendStatus(400);
-//   }
+  if (email === "" || password === "") {
+    res.sendStatus(400);
+  }
+  if (foundEmail === email) {
+    res.sendStatus(400);
+  }
 
-//   users[id] = { id, email, hashedPassword };
-//   res.redirect("/urls");
-// });
+  users[id] = { id, email, hashedPassword };
+  res.redirect("/urls");
+});
 
 module.exports = router;
