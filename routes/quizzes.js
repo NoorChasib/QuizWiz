b22c76ec58f0ccd387937bf6c633e100b3689842
Zak <zak.johansson@gmail.com>
2023-01-04 10:36:54 -0800

    do pseudocode for new quiz

--- routes/quizzes.js
+++ routes/quizzes.js
@@ -4,6 +4,7 @@
 const express = require("express");
 const app = express();
 const router = express.Router();
+const pool = require("./_postgres");
 
 router.get("/list", (req, res) => {
   res.render("quizlist");
@@ -14,7 +15,11 @@ router.get("/new", (req, res) => {
 });
 
 router.post("/new", (req, res) => {
-  
+  //give quiz an id
+  //add question id to each question and push content to question table
+  //add answer id to each answer and push content to answers table
+  //reference questions and answers id to question_answers table
+  //reference question answers id to quiz table
 });
 
 module.exports = router;
