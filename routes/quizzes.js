f4aa931d7ede7af42ffd7a5248f37152f61def69
Zak <zak.johansson@gmail.com>
2023-01-04 10:56:06 -0800

    connect routes to new pages

--- routes/quizzes.js
+++ routes/quizzes.js
@@ -14,12 +14,12 @@ router.get("/new", (req, res) => {
   res.render("quiz_form");
 });
 
-router.post("/new", (req, res) => {
-  //give quiz an id
-  //add question id to each question and push content to question table
-  //add answer id to each answer and push content to answers table
-  //reference questions and answers id to question_answers table
-  //reference question answers id to quiz table
-});
+// router.post("/new", (req, res) => {
+//   //give quiz an id
+//   //add question id to each question and push content to question table
+//   //add answer id to each answer and push content to answers table
+//   //reference questions and answers id to question_answers table
+//   //reference question answers id to quiz table
+// });
 
 module.exports = router;
